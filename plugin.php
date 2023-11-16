2f53e01244957fc218ebc9d8808b56087782f624
Hazem <h.hussien@singularity.finance>
2023-11-16 14:06:59 +0200

    Add attributes for number of posts and display of featured image, and implement render callback function for latest posts block

--- plugin.php
+++ plugin.php
@@ -21,8 +21,44 @@
  *
  * @see https://developer.wordpress.org/block-editor/tutorials/block-tutorial/writing-your-first-block-type/
  */
+
+function blocks_latest_posts_render_callback($attributes, $content)
+{
+	$args = array(
+		'posts_per_page' => $attributes['numberOfPosts'],
+		'posts_status' => 'publish',
+
+	);
+	$recent_posts = get_posts($args);
+	$posts = '<ul ' . get_block_wrapper_attributes() . ' >';
+
+	foreach ($recent_posts as $post) {
+		$title = get_the_title($post->ID);
+		$title = $title ? $title : '(no title)';
+		$permalink = get_the_permalink($post->ID);
+		$excerpt = get_the_excerpt($post->ID);
+		$posts .= '<li>';
+		if ($attributes['displayFeaturedImage'] && has_post_thumbnail($post->ID)) {
+			$posts .= '<a href="' . esc_url($permalink) . '">';
+			$posts .= get_the_post_thumbnail($post->ID, 'thumbnail');
+			$posts .= '</a>';
+		}
+		$posts .= '<h5><a href="' . esc_url($permalink) . '">' . $title . '</a></h5>';
+		$posts .= '<time datetime="' . esc_attr(get_the_date(DATE_W3C, $post->ID)) . '">' . esc_html(get_the_date('', $post->ID)) . '</time>';
+
+		if (!empty($excerpt)) {
+			$posts .= '<p>' . $excerpt . '</p>';
+		}
+		$posts .= '</li>';
+	}
+
+	$posts .= '</ul>';
+	return $posts;
+}
 function create_block_latest_posts_block_init()
 {
-	register_block_type_from_metadata(__DIR__);
+	register_block_type_from_metadata(__DIR__, array(
+		'render_callback' => 'blocks_latest_posts_render_callback'
+	));
 }
 add_action('init', 'create_block_latest_posts_block_init');
--- src/save.js
+++ src/save.js
@@ -1,13 +1,3 @@
-import { __ } from '@wordpress/i18n';
-import { useBlockProps } from '@wordpress/block-editor';
-
 export default function save() {
-	return (
-		<p { ...useBlockProps.save() }>
-			{ __(
-				'latest posts – hello from the saved content!',
-				'latest-posts'
-			) }
-		</p>
-	);
+	return null;
 }
